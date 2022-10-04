feae65440ed31cf6a4de63e9be117619e64f8c50
Errolrabano <108141836+Errolrabano@users.noreply.github.com>
2022-10-04 16:02:47 -0400

    Added a read file and list

--- Quiz.py
+++ Quiz.py
@@ -1,6 +1,19 @@
-import time
-#Welcoming 
-print("Welcome to the game, Are You A Gamer ?")
+import time 
+#Welcoming
+
+def main():
+    testFile = open("testFile.txt", "r")
+
+    line = testFile.readline()
+
+    while line != "":
+        print( line )
+        line = testFile.readline()
+
+
+
+
+time.sleep(2)
 #Chances 
 chances = 1 
 print("You will have", chances, "chance to answer correctly. \nChoose Your Answer Wisely! \n")
@@ -10,6 +23,12 @@ time.sleep(2)
 #Score
 score = -1 
 
+#question list 
+q1 = ["(a)Obviously Not!", "(b)yes", "(c)Nah", "(d)I devote myself to games"] 
+q2 = ["(a)GTA V(a)", "(b)Mario Kart 64 Released: 1996", "(c)2k22", "(d)Outlast"] 
+q3 = ["(a)PC, PlayStation, Xbox, wii","(b)PC, wii, Xbox, PlayStation", "(c)wii, PlayStation, Xbox, PC", "(d)PlayStation, PC, Xbox, wii"] 
+q4 = ["(a)8 Hours","(b)10 Hours", "(c)You Sleep?","(d)24 Hours"] 
+
 #question 0
 
 question_0 = print ("Lets start the game with a simple question, Are You A Gamer?) \n Yes(a) or No(b) \n\n")
@@ -17,7 +36,7 @@ answer_0 = "a"
 for i in range(chances) :
     answer = input("Answer :")
     if (answer.lower() == answer_0):
-        print("We will see, Let's Begin!\n")
+        print("Are you sure about that, We will see!\n")
         score = score + 1 
         break
     else: 
@@ -27,8 +46,9 @@ for i in range(chances) :
 time.sleep(2)
 
 #question 1 
-
-question_1 = print ("1. Do You Love Games ?) \n Obviously Not!(a) \n yes(b) \n Nah(c) \n I devote myself to games(d) \n\n")
+question_1 = print ("1. Do You Love Games ?\n")
+for question1 in range (len(q1)): 
+    print(q1 [question1], end=" \n\n") 
 answer_1 = "d"
 for i in range(chances) :
     answer = input("Answer :")
@@ -45,7 +65,9 @@ time.sleep(2)
 
 #question 2
 
-question_2 = print ("2. Greatest Classical Game) \n GTA V(a) \n Mario Kart 64 Released: 1996(b) \n 2k22(c) \n Outlast(d) \n\n")
+question_2 = print ("2. Greatest Classical Game\n")
+for question2 in range (len(q2)): 
+    print(q2 [question2], end=" \n\n") 
 answer_2 = "b"
 for i in range(chances) :
     answer = input("Answer :")
@@ -62,7 +84,9 @@ time.sleep(2)
 
 #question 3 
 
-question_3 = print ("3. Worst to Best) \n PC, PlayStation, Xbox, wii(a) \n PC, wii, Xbox, PlayStation(b) \n wii, PlayStation, Xbox, PC(c) \n PlayStation, PC, Xbox, wii(d) \n\n")
+question_3 = print ("3. Worst to Best")
+for question3 in range (len(q3)): 
+    print(q3 [question3], end=" \n\n") 
 answer_3 = "a"
 for i in range(chances) :
     answer = input("Answer :")
@@ -79,7 +103,9 @@ time.sleep(2)
 
 #question 4 
 
-question_4 = print ("4. how many hours do you sleep?) \n 8 Hours(a) \n 10 Hours(b) \n You Sleep?(c) \n 24 Hours(d) \n\n")
+question_4 = print ("4. how many hours do you sleep?")
+for question4 in range (len(q4)): 
+    print(q4 [question4], end=" \n\n") 
 answer_4 = "c"
 for i in range(chances) :
     answer = input("Answer :")
@@ -101,7 +127,7 @@ while score >= 4:
 
 while score <= 3: 
     print ("Better luck next time! Your score was", score)
-
+    break
 #Closing 
-print("Thank you for playing the Simple Quiz Game!")
-quit()
+print("Thank you for playing the Game!")
+exit()
